09aae067fe6950c0c9eb412910166eb331f61274
TGSP1997 <Gymisandro@web.de>
2023-05-11 06:30:16 +0200

    repro as package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ioutracker",
+    version="0.0.1",
+    description="IOU Tracker",
+    packages=find_packages(),
+    url="https://github.com/TGSP1997/iou-tracker",
+)
